9aeb33745dc13452ea4927bd1aa545d21b04bc05
marcss-bnajera <mbeteta-2024227@kinal.edu.gt>
2025-08-25 11:51:23 -0600

    Agregando dependencias y agregando el index.xhtml con jsp

---
+++ src/main/java/org/kinscript/registro_Cliente/RegistroClienteWeb.java
@@ -0,0 +1,11 @@
+package org.kinscript.registro_Cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroClienteWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroClienteWeb.class, args);
+    }
+}
